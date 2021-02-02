272aef0eca0d06bfdca697ea42b49b1a6468a3d6
LiberisIvas <liberis.ivas@gmail.com>
2021-02-02 23:27:04 +0100

    adding...

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/"> Your shopping cart </a>
+        
+      </header>
+      <main>Muesli List</main>
+      <footer> All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
